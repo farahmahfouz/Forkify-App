059feda2fc5d90d2b261d4de76238c8326fa1dae
farahmahfouz <farahmahfouz11@gmail.com>
2024-11-27 21:29:25 +0200

    Fix/Fraction Fn

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,6 @@
 import View from './View.js';
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+import { Fraction } from 'fraction';
 console.log(Fraction)
 
 class RecipeView extends View {
